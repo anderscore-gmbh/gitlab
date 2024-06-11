7e82976e82d6ad81ad225f9766019e8758d71c1d
Patrick Ungewiß <patrick.ungewiss@anderscore.com>
2024-06-11 06:31:51 +0200

    Deployskript angepasst

--- examples/abschluss/hello-world/deploy.sh
+++ examples/abschluss/hello-world/deploy.sh
@@ -1,4 +1,4 @@
-echo "Deploying $CONTAINER_RELEASE_IMAGE to test environment."
-docker build -t hello-world-dataport .
-docker run -it -p 9090:9090 hello-world-dataport
-curl localhost:9090
+echo "Deploying $CONTAINER_RELEASE_IMAGE to test environment..."
+docker pull $CONTAINER_RELEASE_IMAGE
+docker run -it -p 9090:9090 $CONTAINER_RELEASE_IMAGE
+echo "Deployed $CONTAINER_RELEASE_IMAGE to test environment."
